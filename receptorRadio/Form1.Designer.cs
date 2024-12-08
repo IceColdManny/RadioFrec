2dddeadc6f5ae12bac7dc206542440413a30aeca
José Manuel Juárez Díaz <91635106+IceColdManny@users.noreply.github.com>
2024-12-08 15:40:36 -0600

    Add files via upload

---
+++ receptorRadio/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace PracticaV3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAlgo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtAlgo
+            // 
+            this.txtAlgo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txtAlgo.Location = new System.Drawing.Point(13, 25);
+            this.txtAlgo.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAlgo.Name = "txtAlgo";
+            this.txtAlgo.Size = new System.Drawing.Size(659, 36);
+            this.txtAlgo.TabIndex = 0;
+            this.txtAlgo.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.ClientSize = new System.Drawing.Size(695, 131);
+            this.Controls.Add(this.txtAlgo);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAlgo;
+    }
+}
+
---
+++ receptorRadio/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace PracticaV3
+{
+    public partial class Form1 : Form
+    {
+        System.IO.Ports.SerialPort Port;
+        bool IsClosed = false;
+        public Form1()
+        {
+            InitializeComponent();
+            Port = new System.IO.Ports.SerialPort();
+            Port.PortName = "COM10";
+            Port.BaudRate = 9600;
+            Port.ReadTimeout = 500;
+            try
+            {
+                Port.Open();
+            }
+            catch
+            {
+
+            }
+
+            Thread Hilo = new Thread(EscuchaSerial);
+            Hilo.Start();
+
+            //ConsoleKey tecla;
+            //tecla = Console.ReadKey(true).Key;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void EscuchaSerial()
+        {
+            while (!IsClosed)
+            {
+                try
+                {
+                    string cadena = Port.ReadLine();
+                    txtAlgo.Invoke(new MethodInvoker(delegate
+                    {
+                        txtAlgo.Text = cadena;
+                    }));
+
+                }
+                catch { }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            IsClosed = true;
+            if (Port.IsOpen)
+            {
+                Port.Close();
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (Port.IsOpen)
+            {
+                Port.Close();
+            }
+
+            base.OnFormClosing(e);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
